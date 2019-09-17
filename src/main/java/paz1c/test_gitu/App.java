347a24d0d3d08692302aa585434410fd46e564da
student <student@158.197.35.97>
2019-09-17 10:37:18 +0200

    blby textik

--- src/main/java/paz1c/test_gitu/App.java
+++ src/main/java/paz1c/test_gitu/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Europe!" );
+        System.out.println("smrad bolo citit aj na sto honov");
     }
 }
