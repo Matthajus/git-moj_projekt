60d70b94922ab2ed5bceae62299b71d87f6cb908
student <student@158.197.35.98>
2019-09-17 09:37:21 +0200

    initial commit

---
+++ src/main/java/paz1c/test_gitu/App.java
@@ -0,0 +1,13 @@
+package paz1c.test_gitu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
