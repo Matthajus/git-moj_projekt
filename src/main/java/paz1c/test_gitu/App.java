bcdbbbea4fee0163458740e3bbc17b5a1f196219
student <student@158.197.35.98>
2019-09-17 11:00:38 +0200

    Git je somarina!  s

--- src/main/java/paz1c/test_gitu/App.java
+++ src/main/java/paz1c/test_gitu/App.java
@@ -11,5 +11,9 @@ public class App
         System.out.println( "Hello Europe!" );
         System.out.println("smrad bolo citit aj na sto honov");
         System.out.println("nefunguje????");
+        
+        for (int i = 0; i < 5; i++) {
+			System.out.println("Git je somarina!!");
+		}
     }
 }
---
+++ src/main/java/paz1c/test_gitu/stara.java
@@ -0,0 +1,5 @@
+package paz1c.test_gitu;
+
+public class stara {
+
+}
