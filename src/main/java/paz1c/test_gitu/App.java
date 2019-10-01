7b14a616335d114f9b6119fc24b727f304d7e493
student <student@158.197.35.98>
2019-10-01 09:24:01 +0200

    metoda minus

--- src/main/java/paz1c/test_gitu/App.java
+++ src/main/java/paz1c/test_gitu/App.java
@@ -4,17 +4,19 @@ package paz1c.test_gitu;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Europe!" );
-        System.out.println("smrad bolo citit aj na sto honov");
-        System.out.println("adwdawd????");
-        System.out.println("kačky");
-    }
-    
-    private static int plus(int a, int b) {
-    	return a+b;
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello Europe!");
+		System.out.println("smrad bolo citit aj na sto honov");
+		System.out.println("adwdawd????");
+		System.out.println("kačky");
+	}
+
+	private static int plus(int a, int b) {
+		return a + b;
+	}
+
+	private static int minus(int a, int b) {
+		return a - b;
+	}
 }
